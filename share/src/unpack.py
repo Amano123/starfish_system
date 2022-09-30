404dc692cf463786f635391f2424ec5b7516a1d0
Hiroto Yano <mi22a012@oecu.jp>
2022-09-30 21:59:45 +0900

    CSVファイル作成プログラムの追加

--- share/src/unpack.py
+++ share/src/unpack.py
@@ -2,19 +2,19 @@ import tarfile
 import os
 
 file_path = "../data/ldcc-20140209.tar.gz"
+save_path = "../data/livedoor"
 
 # 解凍処理
 tar = tarfile.open(file_path)
-tar.extractall("../data/livedoor/")
+tar.extractall(save_path)
 tar.close()
 
 # フォルダのファイルとディレクトリを確認
-files_folders = [name for name in os.listdir("../data/livedoor/text/")]
+files_folders = [name for name in os.listdir(f"{save_path}/text/")]
 print(files_folders)
 
 # カテゴリーのフォルダのみを抽出
-categories = [name for name in os.listdir("../data/livedoor/text/") if os.path.isdir("../data/livedoor/text/" + name)]
+categories = [name for name in os.listdir(f"{save_path}/text/") if os.path.isdir(f"{save_path}/text/" + name)]
 
 print(f"カテゴリー数：{len(categories)}")
 print(categories)
-
